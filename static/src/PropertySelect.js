3ebb347183e672e11750f6fd6b29d90c52c2b622
James Hobin <hobinjk@gmail.com>
2017-08-23 14:53:08 -0400

    Make PropertySelect support selecting

--- static/src/PropertySelect.js
+++ static/src/PropertySelect.js
@@ -4,6 +4,15 @@ function PropertySelect(elt) {
   this.elt.addEventListener('click', this.onClick);
 }
 
-PropertySelect.prototype.onClick = function() {
+PropertySelect.prototype.onClick = function(e) {
   this.elt.classList.toggle('open');
+  let selected = this.elt.querySelector('.selected');
+  if (selected) {
+    if (!selected.dataset.value) {
+      this.elt.removeChild(selected);
+    } else {
+      selected.classList.remove('selected');
+    }
+  }
+  e.target.classList.add('selected');
 };
